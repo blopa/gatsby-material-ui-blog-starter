21ea3e6d36ee3487a0933004f72574c91db54eff
Pablo Benmaman <montenegro.pablo@outlook.com>
2020-08-28 20:29:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# gatsby-material-ui-starter
+
+Based on the work by:
+https://github.com/mui-org/material-ui/tree/master/examples/gatsby
+https://github.com/dominicabela/gatsby-starter-material-ui
+https://github.com/infonomic/material-ui-theme-switcher
---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+const plugins = require('./gatsby-config.plugins');
+
+module.exports = {
+    siteMetadata: {
+        title: 'Gatsby Material UI Starter',
+        description: 'Kick off your next, great Gatsby project with this Material UI starter.',
+        author: '@blopa',
+    },
+    plugins,
+};
