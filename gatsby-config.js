f9664116f55989e5fb24022c8d826a1eafced279
Pablo Benmaman <montenegro.pablo@outlook.com>
2020-08-28 22:16:20 +0200

    add pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,14 @@ const plugins = require('./gatsby-config.plugins');
 const defaultLanguage = 'en';
 
 module.exports = {
+    pathPrefix: 'gatsby-material-ui-blog-starter',
     siteMetadata: {
         title: 'My Blog',
         author: 'blopa',
         summary: 'some summary for myself',
         defaultLanguage,
         description: 'Yet another developer personal blog',
-        siteUrl: 'https://my.blog',
+        siteUrl: 'https://blopa.github.io/gatsby-material-ui-blog-starter/',
         social: {
             twitter: 'mytwitteruser',
         },
